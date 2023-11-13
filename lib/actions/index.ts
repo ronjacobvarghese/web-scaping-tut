21eff99c847eaf4148935980281a5491130ef6b9
Ron Jacob <generalsmoke23@gmail.com>
2023-11-13 22:02:35 +0530

    Hero Carousel Completed

---
+++ lib/actions/index.ts
@@ -0,0 +1,10 @@
+"use server";
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return;
+  try {
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product:${error.message}`);
+  }
+}
